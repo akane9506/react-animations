24b75a5350eab56a313c585b87a5f9c40f7f47e1
akane9506 <akane19950611@gmail.com>
2026-01-11 22:42:14 -0800

    add slider button

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import { Theme } from "@radix-ui/themes";
+import { Theme, Flex } from "@radix-ui/themes";
 // import "@radix-ui/themes/styles.css";
 import "./theme.css";
 import RippleButton from "./components/RippleButton";
+import SliderButton from "./components/SliderButton";
 import Card from "./components/Card";
 
 function App() {
   return (
     <>
-      <Theme accentColor="blue" grayColor="gray">
-        <Card title="Ripple Button">
-          <RippleButton text={"Ripple Button"} duration={1500} />
-        </Card>
-        <Card title="Ripple Button">
-          <RippleButton text={"Ripple Button"} duration={1500} />
-        </Card>
+      <Theme accentColor="blue" grayColor="gray" appearance="inherit">
+        <Flex gap={"3"} wrap={"wrap"}>
+          <Card title="Ripple Button">
+            <RippleButton text={"Ripple Button"} duration={1500} />
+          </Card>
+          <Card title="Slider Button">
+            <SliderButton text={"Slider Button"} duration={300} />
+          </Card>
+        </Flex>
       </Theme>
     </>
   );
--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -8,17 +8,14 @@ type CardProps = {
 export default function Card({ title, children }: CardProps) {
   return (
     <Box className="w-fit">
-      <Box className="relative z-10 w-70 h-70 border border-(--gray-8) border-dashed flex justify-center items-center rounded-2xl bg-(--color-background) shadow-xl">
+      <Box
+        className="relative z-10 w-70 h-70 border border-(--gray-8) border-dashed flex justify-center items-center rounded-2xl bg-(--color-background) shadow-[0_7px_6px_var(--gray-a7)] bg-[radial-gradient(circle,var(--gray-4)_1px,transparent_1px)] 
+bg-size-[12px_12px] "
+      >
         {children}
       </Box>
-      <Box className="relative h-14 -z-10 bg-(--accent-9) -translate-y-3 rounded-b-2xl flex items-end justify-center pb-3">
-        <Text
-          weight="medium"
-          size="3"
-          align="center"
-          as="p"
-          className="text-(--accent-1)"
-        >
+      <Box className="relative h-12 -z-10 bg-(--accent-9) -translate-y-3 rounded-b-2xl flex items-end justify-center pb-2">
+        <Text weight="bold" size="2" align="center" as="p" className="text-(--accent-1)">
           {title}
         </Text>
       </Box>
